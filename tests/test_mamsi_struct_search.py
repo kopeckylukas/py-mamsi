0ac55f35345133cf2eaf0e7ebc45879d94650462
Lukas Kopecky <56082570+kopeckylukas@users.noreply.github.com>
2025-11-17 14:43:02 +0000

    change data import sample data from simulated dummy data

--- tests/test_mamsi_struct_search.py
+++ tests/test_mamsi_struct_search.py
@@ -15,23 +15,6 @@ from pathlib import Path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from mamsi.mamsi_struct_search import MamsiStructSearch
 
-@pytest.fixture
-def sample_lcms_data():
-    data = {
-        'HPOS_233.25_149.111m/z': [100, 200, 300],
-        'HPOS_233.25_150.111m/z': [150, 250, 350],
-        'HPOS_234.25_149.111m/z': [200, 300, 400]
-    }
-    return pd.DataFrame(data)
-
-@pytest.fixture
-def sample_msi_data():
-    data = {
-        'Assay1_149.111': [100, 200, 300],
-        'Assay1_150.111': [150, 250, 350],
-        'Assay2_149.111': [200, 300, 400]
-    }
-    return pd.DataFrame(data)
 
 @pytest.fixture
 def sample_data_lcms():
@@ -46,10 +29,10 @@ def sample_data_msi():
     return pd.read_csv(data_path)
      
 
-def test_load_lcms(sample_lcms_data):
+def test_load_lcms(sample_data_lcms):
     searcher = MamsiStructSearch()
-    searcher.load_lcms(sample_lcms_data)
-    assert searcher.intensities.equals(sample_lcms_data)
+    searcher.load_lcms(sample_data_lcms)
+    assert searcher.intensities.equals(sample_data_lcms)
     assert searcher.feature_metadata is not None
     assert searcher.assay_links is not None
 
@@ -60,9 +43,9 @@ def test_load_msi(sample_data_msi):
     assert searcher.feature_metadata is not None
     assert searcher.assay_links is not None
 
-def test_structural_search_msi(sample_data_msi):
-    searcher = MamsiStructSearch(ppm=10)
-    searcher.load_msi(sample_data_msi)
-    results = searcher.get_structural_clusters(annotate=False)
-    assert isinstance(results, pd.DataFrame)
-    assert not results.empty
+# def test_structural_search_msi(sample_data_msi):
+#     searcher = MamsiStructSearch(ppm=10)
+#     searcher.load_msi(sample_data_msi)
+#     results = searcher.get_structural_clusters(annotate=False)
+#     assert isinstance(results, pd.DataFrame)
+#     assert not results.empty
